73b0cc592d2a770f7a1fea674b0874bc30128dbd
Кондусов Александр Александрович <kondusov_aa@ACADEMY.TOP>
2026-03-13 14:28:18 +0300

    function

---
+++ code/assocArray.js
@@ -0,0 +1,13 @@
+let arr_number = [0,1,2,3,4]
+let arr_text = ['понедельник','вторник','среда']
+
+let exam_result = {
+    'Иван': 5,
+    'Елена': 4,
+    'Олег': 3
+}
+console.log(exam_result.Иван);
+console.log(exam_result);
+exam_result['Ирина'] = 5;
+exam_result.Олег = 5;
+console.log(exam_result);
